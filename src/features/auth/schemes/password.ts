3c3f0162bf306e65f036754ade086a56c4e16fbd
qkransrb <qkransrb90@gmail.com>
2025-08-08 22:00:49 +0900

    feat: auth validation schemes

---
+++ src/features/auth/schemes/password.ts
@@ -0,0 +1,24 @@
+import Joi, { ObjectSchema } from 'joi';
+
+const emailSchema: ObjectSchema = Joi.object().keys({
+  email: Joi.string().required().email().messages({
+    'string.base': 'Email must be of type string',
+    'string.email': 'Email must be valid',
+    'string.empty': 'Email is a required field'
+  })
+});
+
+const passwordSchema: ObjectSchema = Joi.object().keys({
+  password: Joi.string().required().min(4).max(8).messages({
+    'string.base': 'Password must be of type string',
+    'string.min': 'Invalid password',
+    'string.max': 'Invalid password',
+    'string.empty': 'Password is a required field'
+  }),
+  confirmPassword: Joi.string().required().valid(Joi.ref('password')).messages({
+    'any.only': 'Passwords should match',
+    'any.required': 'Confirm password is a required field'
+  })
+});
+
+export { emailSchema, passwordSchema };
